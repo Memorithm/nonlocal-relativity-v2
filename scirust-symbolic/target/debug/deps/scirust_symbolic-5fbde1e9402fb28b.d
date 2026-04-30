4cf4daf0f4ce9ed7665884cd03393e9aa1b6c32a
Tarek <tarek@avid.dev>
2026-04-30 10:27:27 +0200

    chore: remove build artifacts and backup files from tracking

--- scirust-symbolic/target/debug/deps/scirust_symbolic-5fbde1e9402fb28b.d
+++
@@ -1,5 +0,0 @@
-/root/scirust/scirust-symbolic/target/debug/deps/scirust_symbolic-5fbde1e9402fb28b.d: src/lib.rs
-
-/root/scirust/scirust-symbolic/target/debug/deps/libscirust_symbolic-5fbde1e9402fb28b.rmeta: src/lib.rs
-
-src/lib.rs:
